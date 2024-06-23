8e1658fb4e8ae97886b948dd968614c070698bdd
JRgit03 <3411770569@qq.com>
2024-06-23 23:42:08 +0800

    feat： 首次创建

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/controller/RestController.java
@@ -0,0 +1,17 @@
+package com.ruoyi.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @Value("${common.value}")
+    private String commonValue;
+
+    @GetMapping
+    public String t1(){
+        return commonValue;
+    }
+}
